e0f1c99b57c0c81cee8d09f8fd88ac58b9aa48cc
Matheus Rodrigues Guedes <matheusguedes25@gmail.com>
2021-05-08 18:19:14 -0300

    Security Config

---
+++ backend/src/main/java/com/devsuperior/dsvendas/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.devsuperior.dsvendas.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+/*
+ * Classe para configuração de segurança 
+ * 
+ * --> habilitando o cors (Cross-Origin Resource Sharing), é um recurso de cabeçalhos adicionais na requisição http, que
+ * 	 permite o aplicativo ser executado no domínio com permissão de acesso de recursos selecionados do servidor;
+ * --> tira a tela padrão de login do security
+ * 
+ * @Configuration --> Indica que a classe é de configuração do projeto
+ * */
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private Environment env;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		//aqui é liberado o aplicativo de geranciamento banco de dados (H2)
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		
+		//É utilizado o cors criado, liberando o acesso e que a aplicação n manterá estado (por ser rest)
+		http.cors().and().csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.authorizeRequests().anyRequest().permitAll();
+	}
+
+	//é contruido um objeto para configuração do cors, liberando o acesso para o frontend
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+}
